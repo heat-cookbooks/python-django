094a4476b48f0cf56c0cbf85ecb95a57a6077f01
Jason Boyles <jason@duncancreek.net>
2015-04-07 15:56:02 -0500

    lock mysql to 5.3.6

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures python-django'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ apt mysql build-essential python apache2 }.each do |cb|
+%w{ apt build-essential python apache2 }.each do |cb|
 	depends cb
 end
+
+depends 'mysql', '5.3.6'
